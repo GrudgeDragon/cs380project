2d7783684eeb7e6da6f785fb9d6a6169b686b105
Christopher Eicher <christopher.eicher1@gmail.com>
2017-11-21 15:38:39 -0800

    Better stucture and documentation

--- ProceduralMeshes/Assets/MeshGenerator.cs
+++ ProceduralMeshes/Assets/MeshGenerator.cs
@@ -6,64 +6,67 @@ using UnityEngine;
 public class MeshGenerator : MonoBehaviour
 {
 
-    //public Vector3[] verts;
+    // temp lists to hold the verts and faces
     public List<Vector3> vertList;
-    //public Vector2[] uvs;
-    //public int[] triangles;
     public List<int> faceList;
 
     void Start()
     {
-        
+        // get the mesh and reset empty it of data
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         mesh.Clear();
 
-        int ringVertCount = 10;
-        int numHeightSegments = 60;
-        float tubeWidth = 1f;
-        float heightStep = 0.2f;
-        //Matrix4x4 rotateBy = Matrix4x4.Rotate(Quaternion.AngleAxis(5, new Vector3(1,0,0)));
-        Matrix4x4 rotation = Matrix4x4.identity;
-        float helixRadius = 2;
+        // some variables for tweaking
+        int ringVertCount = 10;     // number of verts in the rings
+        int numHeightSegments = 60; // number of rings that make up the helix
+        float tubeWidth = 1f;       // minor radius
+        float heightStep = 0.2f;    // distance to next ring in the helix
+        float helixRadius = 2;      // major radius
  
+        // index of the last ring added to the mesh
         int prevRing = -1;
+
+        // for each ring that will make up the helix
         for (int i = 0; i < numHeightSegments; ++i)
         {
+            // t is our main paremeter
             float t = i * heightStep;
+
+            // helix equation and its derivitive are used to get position and direction
             Vector3 pos = new Vector3(helixRadius * Mathf.Cos(t), t, helixRadius * Mathf.Sin(t));
             Vector3 dir = new Vector3(helixRadius * -Mathf.Sin(t), 0, helixRadius * Mathf.Cos(t));
-            rotation = Matrix4x4.Rotate(Quaternion.LookRotation(Vector3.up, dir));
-            
+
+            // use position and direction to create our local matrices
             Matrix4x4 trans = Matrix4x4.Translate(pos);
-            int ring = AddRing(trans * rotation, ringVertCount, tubeWidth);
-            //secondRing = AddRing(Matrix4x4.Translate(new Vector3(0, i + 1, 0)) * rotation, ringVertCount, 1);
+            Matrix4x4 rotation = Matrix4x4.Rotate(Quaternion.LookRotation(Vector3.up, dir));
+
+            // add a ring to the mesh
+            int ring; // index of the first vert in a ring
+            ring = AddRing(trans * rotation, ringVertCount, tubeWidth);
+
+            // if this is not the first ring
             if (prevRing != -1)
-                AddBand(prevRing, ring, ringVertCount);
+                AddBand(prevRing, ring, ringVertCount); // make a band of triangles from this and the last ring
 
             prevRing = ring;
-            //rotation *= rotateBy;
         }
-
- 
-
-        Vector3[] thing = (vertList.ToArray());
-        mesh.vertices = thing;
+       
+        // assign to the mesh
+        mesh.vertices = vertList.ToArray();
         mesh.triangles = faceList.ToArray();
-        mesh.RecalculateNormals();
-
-        Debug.Log("Hi!");
+        mesh.RecalculateNormals(); // without this we get no light
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
 
-    }
+    //}
 
     int AddRing(Matrix4x4 trans, int numPoints, float radius)
     {
-        Debug.Log(trans);
+        //Debug.Log(trans);
 
         // remember the index of the first vert
         int firstVert = vertList.Count;
@@ -86,21 +89,27 @@ public class MeshGenerator : MonoBehaviour
         return firstVert;
 
     }
+
+    // creates a band of quads between two sets of points
     int AddBand(int topEdge, int bottomEdge, int num)
     {
+        // get what will be the index of the first face to be added
         int firstFace = faceList.Count;
+
+        // for quad that will make up the band
         for (int i = 0; i < num; ++i)
         {
+            // first quad triangle
             faceList.Add(topEdge + i);
             faceList.Add(bottomEdge + i);
             faceList.Add(bottomEdge + (i + 1) % num);
+
+            // second quad triangle
             faceList.Add(topEdge + i);
             faceList.Add(bottomEdge + (i + 1) % num);
             faceList.Add(topEdge + (i + 1) % num);
         }
         return firstFace;
-
-
     }
 
 
